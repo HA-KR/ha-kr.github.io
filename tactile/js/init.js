103a1f7001aeef95a8be2b6f17f8defbeb615636
harikrishnan <harikrishnan.a@infibeam.net>
2015-06-29 23:07:06 +0530

    Tactile: Added slick carousel on home page

--- tactile/js/init.js
+++ tactile/js/init.js
@@ -1,5 +1,14 @@
 (function($){
   $(function(){
+    $('.slideshow').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      autoplaySpeed: 1000,
+      slidesToShow: 1,
+      autoplay: true,
+      lazyLoad: 'ondemand'
+    });
     $('.button-collapse').sideNav({menuWidth: 200});
     $('.modal-trigger').leanModal();
   }); // end of document ready
