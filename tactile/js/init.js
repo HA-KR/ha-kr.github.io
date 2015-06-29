347fd68817bea7d9eee5c02b741fa1fadce7a7fa
harikrishnan <harikrishnan.a@infibeam.net>
2015-06-29 21:45:16 +0530

    Tactile web app front-end preview

---
+++ tactile/js/init.js
@@ -0,0 +1,6 @@
+(function($){
+  $(function(){
+    $('.button-collapse').sideNav({menuWidth: 200});
+    $('.modal-trigger').leanModal();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
